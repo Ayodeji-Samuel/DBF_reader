4c283a9752cb2df17e82b408577f8f072d32b36a
Ayodeji <makinde6@gmail.com>
2018-07-25 09:58:54 +0100

    Comments added for better understanding of the code

--- dbf_reader_v3.py
+++ dbf_reader_v3.py
@@ -1,13 +1,16 @@
 ########################################################################
-#      By      Ayodeji Makinde
+#     This code can be edit and extend to make it better and robust
+#
+#  By      Ayodeji Makinde          Thank you
 ########################################################################
+
 
 from dbfread import DBF
 from pandas import DataFrame
 from glob import glob
 from matplotlib.pyplot import plot, show, xlabel, ylabel, legend
 
-
+# The Iteration start from 1 instead of 0... this is because 0 has been used at the dbfile_data_header() to get the Header name with the data in it
 def dbfile_data_frame():
     for i in range(1,60):
         for dbf_file in glob(r'data\Ras_{}.dbf'.format(i)):
@@ -17,6 +20,8 @@ def dbfile_data_frame():
                 dataframe_csv(frame)
 
 
+# This was used in order to capture the header name. However, arcpy library could have been the best to use but due to
+# license issues with arcgis I decided to do it manually without affecting anything
 def dbfile_data_header():
     for dbf_file in glob(r'data\Ras_0.dbf'):
         with DBF(dbf_file) as dbf:
